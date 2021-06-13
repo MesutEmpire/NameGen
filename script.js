d24dd516d88d033e4e3a6a0b592c7653ce21399a
Samuel Wainaina <samwainaina01@gmail.com>
2021-06-13 21:14:05 +0300

    Add Formula

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function generateName() {
-    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var day = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    var dob = new Date(document.getElementById("DateOfBirth").value);
-    var gender = document.getElementById("Gender").value;
-    var year = dob.getFullYear().toString();
-  
-    var CC = year.slice(0, 2);
-    var YY = year.slice(2, 4);
-    var MM = dob.getMonth() + 1;
-    var DD = dob.getDate();
-   
-  
-   }
-  
+  var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var dob = new Date(document.getElementById("DateOfBirth").value);
+  var gender = document.getElementById("Gender").value;
+  var year = dob.getFullYear().toString();
+
+  var CC = year.slice(0, 2);
+  var YY = year.slice(2, 4);
+  var MM = dob.getMonth() + 1;
+  var DD = dob.getDate();
+  var DY = dob.getDay();
+
+  var d = (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
+}
